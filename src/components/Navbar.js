ca06f14ee9115347515be3fa743f4fcf459f6b6e
yogesh-haryana <personalyogesh352@gmail.com>
2022-11-15 16:29:08 +0530

    NavBar Active Button Status

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,13 @@ import { NavLink } from 'react-router-dom';
 import useStyles from './NavStyles';
 
 function Navbar() {
+  const [isActive, setActive] = useState('');
   const classes = useStyles();
-
-  const [isActive, setActive] = useState('home');
   return (
     <div>
       <div className={classes.NavbarContainer}>
-        <NavLink className={isActive === classes.home ? `${classes.navLinks} ${classes.active}` : classes.navLinks} onClick={() => setActive(classes.home)} to="/">Home</NavLink>
-        <NavLink className={isActive === classes.form ? `${classes.navLinks} ${classes.active}` : classes.navLinks} onClick={() => setActive(classes.form)} to="/form">Add New User</NavLink>
+        <NavLink className={isActive === 'home' ? `${classes.navLinks} ${classes.active}` : classes.navLinks} onClick={() => setActive('home')} to="/">Home</NavLink>
+        <NavLink className={isActive === 'form' ? `${classes.navLinks} ${classes.active}` : classes.navLinks} onClick={() => setActive('form')} to="/form">Add New User</NavLink>
       </div>
     </div>
   );
